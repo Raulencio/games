9b0f0ded37b3898f4905b2bb750eb9d8e2ce4fdd
Raulencio <47399093+Raulencio@users.noreply.github.com>
2020-08-08 15:06:58 -0400

    Add files via upload

--- nuevojs.js
+++ nuevojs.js
@@ -10,7 +10,9 @@ pNa=[true,7,1,6,2],pNa=[true,7,2,6,2],pNa=[true,7,3,6,2],pNa=[true,7,4,6,2],pNa=
 
 var colorA="#13e900";//verde
 var colorB="#0013bd";//azul
-var colores=[colorA,"#e6f517","#e40e0a",colorB,colorA,"#e6f517","#e40e0a",colorB]
+var colores=[colorA,"#e6f517","#e40e0a",colorB,colorA,"#e6f517","#e40e0a",colorB,"#000","#fff"];
+var colorEa=colores[8];
+var colorEb=colores[9];
 //unavez=false;
 for(var cr=1;cr<9;cr++){
     document.getElementById("cr"+cr).style.backgroundColor=colores[cr-1];    
@@ -24,48 +26,58 @@ function randomAr(aM, bM) {
 
     return num;
 }
+var mov=1;
 function al(){
-    if(unavez){
+    //if(unavez){
+        if(mov==1){
+        colorEb=colores[9];
+        colorEa=colores[8];mov++;
+        }else{            
+        colorEb=colores[8];
+        colorEa=colores[9];mov=1;
+//        }
+        }
 elC(randomAr(4,0));
-elC(randomAr(8,4));}
+elC(randomAr(8,4));
 }
 al();
+fun();
 
 function elC(n){
-    if(unavez){        
+    //if(unavez){        
         if(n<5){for(var k=1;k<5;k++){document.getElementById("cr"+k).style.backgroundColor=colores[k-1];}
         }else{for(var k=5;k<9;k++){document.getElementById("cr"+k).style.backgroundColor=colores[k-1];}}
     
         switch(n){
-            case 1:colorA=colores[n-1];cnA="Verde";document.getElementById("cr1").style.backgroundColor="#000";break;
-            case 2:colorA=colores[n-1];cnA="Amarillo";document.getElementById("cr2").style.backgroundColor="#000";break;
-            case 3:colorA=colores[n-1];cnA="Rojo";document.getElementById("cr3").style.backgroundColor="#000";break;
-            case 4:colorA=colores[n-1];cnA="Azul";document.getElementById("cr4").style.backgroundColor="#000";break;
-            case 5:colorB=colores[n-1];cnB="Verde";document.getElementById("cr5").style.backgroundColor="#fff";break;
-            case 6:colorB=colores[n-1];cnB="Amarillo";document.getElementById("cr6").style.backgroundColor="#fff";break;
-            case 7:colorB=colores[n-1];cnB="Rojo";document.getElementById("cr7").style.backgroundColor="#fff";break;
-            case 8:colorB=colores[n-1];cnB="Azul";document.getElementById("cr8").style.backgroundColor="#fff";break;    
+            case 1:colorA=colores[n-1];cnA="Verde";document.getElementById("cr1").style.backgroundColor=colorEa;break;
+            case 2:colorA=colores[n-1];cnA="Amarillo";document.getElementById("cr2").style.backgroundColor=colorEa;break;
+            case 3:colorA=colores[n-1];cnA="Rojo";document.getElementById("cr3").style.backgroundColor=colorEa;break;
+            case 4:colorA=colores[n-1];cnA="Azul";document.getElementById("cr4").style.backgroundColor=colorEa;break;
+            case 5:colorB=colores[n-1];cnB="Verde";document.getElementById("cr5").style.backgroundColor=colorEb;break;
+            case 6:colorB=colores[n-1];cnB="Amarillo";document.getElementById("cr6").style.backgroundColor=colorEb;break;
+            case 7:colorB=colores[n-1];cnB="Rojo";document.getElementById("cr7").style.backgroundColor=colorEb;break;
+            case 8:colorB=colores[n-1];cnB="Azul";document.getElementById("cr8").style.backgroundColor=colorEb;break;    
         }
     document.getElementById("elpo").textContent=cnA+" vs "+cnB;
         if(n<5){
             for(var i=1;i<5;i++){
                 if(i==n){document.getElementById("color"+i).style.backgroundColor=colorA;}
-                else{document.getElementById("color"+i).style.backgroundColor="#000";}        
+                else{document.getElementById("color"+i).style.backgroundColor=colorEa;}        
             }
         }else{
             for(var i=5;i<9;i++){
                 if(i==n){document.getElementById("color"+i).style.backgroundColor=colorB;}
-                else{document.getElementById("color"+i).style.backgroundColor="#fff";}  
+                else{document.getElementById("color"+i).style.backgroundColor=colorEb;}  
             }
         }
-    }
+    //}
 }
 
 function fun(){
     if(unavez){
-        for(var x=1;x<9;x++){
-        document.getElementById("color"+x).hidden=true;        
-        }
+        // for(var x=1;x<9;x++){
+        // document.getElementById("color"+x).hidden=true;        
+        // }
         
         for(var i=1;i<9;i++){
             eldiv.innerHTML+=("<div class='divB' id='id"+i+"'></div>");
@@ -191,57 +203,47 @@ function todos(){
 }
 
 function cambio(y,x,z,a){
-    if(a==1){ 
-    var elp=document.getElementById(y+"a"+x);    
-    elp.style.backgroundColor="#000";
-    elp.style.boxShadow=("2px 2px 5px "+colorA+",-2px -2px 5px "+colorA+",-2px 2px 5px "+colorA+",2px -2px 5px "+colorA+"");
+
     var elp=document.getElementById(y+"b"+x);
+    var elpe=document.getElementById(y+"a"+x);
+
+    if(a==1){
+    elpe.style.backgroundColor=colorEa;
+    elpe.style.boxShadow=("2px 2px 5px "+colorA+",-2px -2px 5px "+colorA+",-2px 2px 5px "+colorA+",2px -2px 5px "+colorA+"");
     elp.style.backgroundColor=colorA;           
     }else{
-    var elp=document.getElementById(y+"a"+x);
-    elp.style.backgroundColor="#fff";
-    elp.style.boxShadow=("2px 2px 5px "+colorB+",-2px -2px 5px "+colorB+",-2px 2px 5px "+colorB+",2px -2px 5px "+colorB+"");
-    var elp=document.getElementById(y+"b"+x);
+    elpe.style.backgroundColor=colorEb;
+    elpe.style.boxShadow=("2px 2px 5px "+colorB+",-2px -2px 5px "+colorB+",-2px 2px 5px "+colorB+",2px -2px 5px "+colorB+"");
     elp.style.backgroundColor=colorB;
     }
+
+    
 switch(z){
-    case 1:
-        var elp=document.getElementById(y+"b"+x);
-        elp.style.borderRadius="0px 0px 0px 0px";
-        var elp=document.getElementById(y+"a"+x);
-        elp.style.borderRadius="0px 0px 0px 0px";
+    case 1:        
+        elp.style.borderRadius="0px 0px 0px 0px";        
+        elpe.style.borderRadius="0px 0px 0px 0px";
         break;//torre
-    case 2:
-        var elp=document.getElementById(y+"b"+x);
-        elp.style.borderRadius="20px 0px 0px 0px";
-        var elp=document.getElementById(y+"a"+x);
-        elp.style.borderRadius="20px 0px 0px 0px";
+    case 2:        
+        elp.style.borderRadius="20px 0px 0px 0px";        
+        elpe.style.borderRadius="20px 0px 0px 0px";
         break;//caballo
-    case 3:
-         var elp=document.getElementById(y+"b"+x);
-                elp.style.borderRadius="0px 10px 0px 10px";
-                var elp=document.getElementById(y+"a"+x);
-                elp.style.borderRadius="0px 10px 0px 10px";
+    case 3:        
+        elp.style.borderRadius="0px 10px 0px 10px";        
+        elpe.style.borderRadius="0px 10px 0px 10px";
         break;//alfil
-    case 4:
-    var elp=document.getElementById(y+"b"+x);
-    elp.style.borderRadius="0px 0px 15px 15px";
-    var elp=document.getElementById(y+"a"+x);
-    elp.style.borderRadius="0px 0px 15px 15px";
+    case 4:        
+        elp.style.borderRadius="0px 0px 15px 15px";
+        elpe.style.borderRadius="0px 0px 15px 15px";
         break;//reina
     case 5:
-        var elp=document.getElementById(y+"b"+x);
         elp.style.borderRadius="15px 15px 0px 0px";
-        var elp=document.getElementById(y+"a"+x);
-        elp.style.borderRadius="15px 15px 0px 0px";
+        elpe.style.borderRadius="15px 15px 0px 0px";
         break;//rey
     case 6:
-        var elp=document.getElementById(y+"b"+x);
-            elp.style.borderRadius="25px";
-            var elp=document.getElementById(y+"a"+x);
-            elp.style.borderRadius="25px"; 
+        elp.style.borderRadius="25px";
+        elpe.style.borderRadius="25px"; 
         break;//peon
-}
+    }
 }
 function decime(x,y){
     for(var i=piezas.length-1;i>=0;i--){
