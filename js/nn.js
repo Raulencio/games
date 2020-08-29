c6ce364e4ad81f5bcafb1485a54fa802cd50e9e1
Raulencio <47399093+Raulencio@users.noreply.github.com>
2020-08-28 21:33:11 -0400

    Add files via upload

--- js/nn.js
+++ js/nn.js
@@ -1,13 +1,11 @@
 function colorTodo(x){
-    try{
+ 
     document.getElementById("planeta"+x).style.backgroundColor=colorRan();
     document.getElementById("satelite"+x).style.backgroundColor=colorRan();
     document.getElementById("universo"+x).style.backgroundColor=colorRan();
     document.getElementById("satelite"+x).style.boxShadow="0px 0px "+randomAr(10,1)+"px "+randomAr(10,1)+"px "+colorRan();
     document.getElementById("planeta"+x).style.boxShadow="0px 0px "+randomAr(25,5)+"px "+randomAr(25,5)+"px "+colorRan();
-    }catch{
 
-    }
     var num=randomAr(10,0);
     if(num==7){
     document.getElementById("planeta"+x).style.boxShadow="0px 0px "+randomAr(50,10)+"px "+randomAr(25,10)+"px "+colorRan();
@@ -67,12 +65,15 @@ function mas(n){var eltea="";
         iabl.style.top=randomAr(800,2)+"px";
         iabl.style.left=randomAr(600,2)+"px";
     }else if(n==3){
-        for(var e=cte;e>1;e--){
+        for(var e=cte;e>1;e--){            
+            document.getElementById("es"+e).style.width=randomAr(5,1)+"px";
+            document.getElementById("es"+e).style.height=randomAr(5,1)+"px";
             document.getElementById("es"+e).style.top=randomAr(800,10)+"px";
             document.getElementById("es"+e).style.left=randomAr(600,10)+"px";
-            document.getElementById("es"+e).style.boxShadow="0px 0px "+randomAr(5,1)+"px "+randomAr(2,1)+"px "+colorRan();
-            document.getElementById("es"+e).style.boxShadow="0px 0px "+randomAr(5,1)+"px "+randomAr(2,1)+"px "+colorRan();
+            document.getElementById("es"+e).style.boxShadow="0px 0px "+randomAr(7,1)+"px "+randomAr(1,0)+"px "+colorRan();
+            document.getElementById("es"+e).style.boxShadow="0px 0px "+randomAr(7,1)+"px "+randomAr(1,0)+"px "+colorRan();
         }
     }
     
 }
+setInterval("mas(2)",10000);
