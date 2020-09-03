bc679e3dd679bc7cceb0f5f0657d572e447fc43e
Raulencio <47399093+Raulencio@users.noreply.github.com>
2020-09-03 00:18:34 -0400

    Add files via upload

--- js/skt1skrip.js
+++ js/skt1skrip.js
@@ -1,6 +1,6 @@
 var iable=document.getElementById("did0");
 iable.style.backgroundColor="#f0f0f0";
-iable.style.width="1000px";
-iable.style.height="1000px";
+iable.style.width="470px";
+iable.style.height="770px";
 
 iable.innerHTML+=generarDiv(11,7,"fila","cuadro");
