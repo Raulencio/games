c8f7081ac1c28098d351e1abbfe5626a96ec4a5f
Raulencio <47399093+Raulencio@users.noreply.github.com>
2020-09-12 20:36:06 -0300

    Add files via upload

--- js/anm.js
+++ js/anm.js
@@ -3,7 +3,7 @@ var colorEqA=colorRan(),colorEqB=colorRan();
 var div=[
 [document.getElementById("div0"),colorRan(),990,1200,0,0,false],
 [document.getElementById("div1"),colorEqA,300,300,755,90,false,randomAr(10,1)],
-[document.getElementById("div2"),colorEqB,300,300,135,600,false,randomAr(6,3)],
+[document.getElementById("div2"),colorEqB,300,300,135,600,false,randomAr(10,1)],
 [document.getElementById("div3"),colorEqB,700,45,45,245,false],
 [document.getElementById("div4"),colorEqA,700,45,1100,45,false],
 [document.getElementById("div5"),colorEqB,420,300,135,90,true],
@@ -23,23 +23,23 @@ div[f][0].hidden=div[f][6];
 if(div[3][2]>1&&div[4][2]>1){
 div[3][2]-=div[1][7];
 div[3][5]+=div[1][7];
-div[4][2]-=div[2][7];}
+div[4][2]-=div[2][7];
+}
 }
 setInterval("juntos()",1000);
 
 console.log(div);
 
-function nose(n){
-if(n==1){div[1][7]++;
+function nose(nen){
+if(nen=="1"){div[1][7]++;
     if(div[6][6]){
     div[6][6]=false;
     }else{
         div[6][6]=true;
     }
-}
-if(n==2){div[2][7]--;
+}else if(nen=="2"){if(div[2][7]>1){div[2][7]--;}
     if(div[5][6]){
-    div[5][6]=false;
+    div[5][6]=false;if(div[2][7]>1){div[2][7]--;}
     }else{
         div[5][6]=true;
     }
