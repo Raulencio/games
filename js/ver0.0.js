8fc48869450f04c07c3490bf3afd95e9d36b9593
Raulencio <47399093+Raulencio@users.noreply.github.com>
2021-02-13 23:08:18 -0300

    Add files via upload

--- js/ver0.0.js
+++ js/ver0.0.js
@@ -16,28 +16,32 @@ function verificarNombre(){
 
 
 var divs=[
-    [$("#fondo"),"absolute","1px","1px","900px","420px",
-    "red","0px 0px 0px 0px red"],
+    [$("#fondo"),"absolute",1,1,900,420,
+    "green","0px 0px 5px 1px",colorRan()],
 
-    [$("#mensaje"),"absolute","10px","50px","800px","400px",
-    "green","0px 0px 10px 1px yellow"]
+    [$("#mensaje"),"absolute",10,50,800,400,
+    "yellow","0px 0px 10px 1px ","red"]
 
 ]
 
-$(document).ready(function(){
-for(var i=0;i<divs.length;i++){
-    console.log(divs[i][0]);
-        divs[i][0].css("position",divs[i][1]);
-        divs[i][0].css("top",divs[i][2]);
-        divs[i][0].css("left",divs[i][3]);
-        divs[i][0].css("width",divs[i][4]);
-        divs[i][0].css("height",divs[i][5]);
-        divs[i][0].css("backgroundColor",divs[i][6]); 
-        divs[i][0].css("boxShadow",divs[i][7]);
-        divs[i][0].css("z-index",i)
-
+function verDivs(){
+    for(var i=0;i<divs.length;i++){
+        console.log(divs[i][0]);
+            divs[i][0].css("position",divs[i][1]);
+            divs[i][0].css("top",divs[i][2]+"px");
+            divs[i][0].css("left",divs[i][3]+"px");
+            divs[i][0].css("width",divs[i][4]+"px");
+            divs[i][0].css("height",divs[i][5]+"px");
+            divs[i][0].css("backgroundColor",divs[i][6]); 
+            divs[i][0].css("boxShadow",divs[i][7]+divs[i][8]);
+            divs[i][0].css("z-index",i)
+    
+    }
 }
 
+$(document).ready(function(){
+
+verDivs();
 verificarNombre();
 
 $("#titulomensaje").text("Binvenido "+nombre);//set 
