a4f119a56b7b053883f64f4a2345b3f74f76ff61
Raulencio <47399093+Raulencio@users.noreply.github.com>
2021-02-14 00:50:16 -0300

    Add files via upload

--- js/ver0.0.js
+++ js/ver0.0.js
@@ -26,7 +26,7 @@ var divs=[
     [$("#cargo"),"absolute",220,10,200,40,
     colorRan()+"aa","0px 0px 5px 1px",colorRan()],
 
-    [$("#planeta"),"absolute",randomAr(270,70),randomAr(660,540),dPlaneta*2,dPlaneta*2,
+    [$("#planeta"),"absolute",randomAr(240,70),randomAr(660,540),dPlaneta*2,dPlaneta*2,
     colorRan(),"0px 0px 5px 1px",colorRan()],
 
     [$("#mensaje"),"absolute",10,50,800,400,
@@ -54,12 +54,12 @@ function moverEstrellas(){
     for(var u=0;u<nEstrellas;u++){
         var estrellita=document.createElement("div");
         estrellita.style.position=("absolute");
-        estrellita.style.top=randomAr(300,20)+"px";
-        estrellita.style.left=randomAr(800,300)+"px";
+        estrellita.style.top=randomAr(400,20)+"px";
+        estrellita.style.left=randomAr(880,240)+"px";
         estrellita.style.width="1px";
         estrellita.style.height="1px";
         estrellita.style.backgroundColor="white";
-        estrellita.style.boxShadow=" 0px 0px"+randomAr(10,1)+"px "+randomAr(5,1)+"px "+colorRan();
+        estrellita.style.boxShadow=" 0px 0px"+randomAr(10,5)+"px "+randomAr(6,2)+"px "+colorRan();
         document.querySelector("#fondo").appendChild(estrellita);
     
     }
@@ -72,8 +72,10 @@ verDivs();
 verificarNombre();
 moverEstrellas();
 $("#planeta").css("border-radius",dPlaneta+"px "+dPlaneta+"px "+dPlaneta+"px "+dPlaneta+"px");
+$("#planeta").css("background","linear-gradient("+colorRan()+","+colorRan()+","+colorRan()+")");
 $("#titulomensaje").text("Binvenido "+nombre);//set 
 $("#textomensaje").text("Que hay "+cargo);//set 
 $("#pCargoNombre").text(cargo+" - "+nombre);
 
 })
+// document.getElementById("enemigo").style.backgroundColor="#"+(Math.floor(Math.random()*16777215).toString(16));
