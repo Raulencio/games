b7ff84142e00873306bfd065514ebe0be48bd7f3
Raulencio <47399093+Raulencio@users.noreply.github.com>
2021-02-25 22:36:18 -0300

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -8,7 +8,7 @@ async function cargarPokemon(url){
 }
 
 async function click(){
-    var numero=Math.floor((Math.random()*150)+1);
+    var numero=Math.floor((Math.random()*809)+1);
     var url="https://pokeapi.co/api/v2/pokemon/"+numero;
 
     var nombrepokemon=await cargarPokemon(url);
@@ -20,71 +20,3 @@ window.onload=function(){
     $("#btnCargar").click(click);    
 
 }
-
-
-
-
-
-
-
-/*
-
-fetch("archivo1.txt")
-.then(data=>data.text())
-.then(contenido=>console.log(contenido))
-.catch(e=>console.log(e.text()))
-
-
-
-function cargarHTML(datos){
-
-    //$("#principal").append(datos);
-    document.getElementById("principal").innerHTML=datos;
-
-}
-function escribirError(e){
-    console.log(e.text());
-}
-
-fetch("archivo1.txt")
-.then(respuesta=>respuesta.text())
-.then(cargarHTML)
-.catch(escribirError);
-
-async function leerDatos(){
-try{
-var respuesta= await fetch("archivo1.txt");
-var datos= await respuesta.text();
-console.log("datos");
-console.log(datos);
-}catch(error){
-    console.log(error);
-}
-}
-
-leerDatos();
-
-
-function escribirRespuesta(data){
-    console.log("respuesta:");
-    console.log(data);
-    console.log("---------------------------------------------");
-    return data.text();//<--el then siempre espera una respuesta (dato)
-    //opción 2, eliminando el segundo then del fetch
-    //data.text().then(escribirTextoRespuesta);
-    }
-    
-    
-    function escribirTextoRespuesta(data){
-    console.log("datos extraídos:");
-    console.log(data);
-    }
-    function escribirError(error){
-    console.log("error generado"+ error);
-    }
-    
-    fetch("mensaje.txt")
-    .then(escribirRespuesta)
-    .then(escribirTextoRespuesta)
-    .catch(escribirError);
-    */
