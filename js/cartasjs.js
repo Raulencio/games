de9ea590ac9805b8651f2e4344908404a7ad1053
Raulencio <47399093+Raulencio@users.noreply.github.com>
2024-06-13 14:26:49 -0400

    Add files via upload

--- js/cartasjs.js
+++ js/cartasjs.js
@@ -20,7 +20,7 @@ var pjs=[
         id: 1,nivel:1,
         nombre:"Samurai",
         tipo: "Espadachin",
-        vida: 2000,
+        vida: 2000,vidaAc:2000,
         ataque:200,
         defensa:100
    },
@@ -28,7 +28,7 @@ var pjs=[
     id: 2,nivel:1,
     nombre:"Ninja",
     tipo: "Espadachin",
-    vida: 1500,
+    vida: 1500,vidaAc:1500,
     ataque:250,
     defensa:80
 },
@@ -36,7 +36,7 @@ var pjs=[
     id: 3,nivel:1,
     nombre:"Arquero",
     tipo: "Rango",
-    vida: 1000,
+    vida: 1000,vidaAc:1000,
     ataque:300,
     defensa:50
 },
@@ -44,7 +44,7 @@ var pjs=[
 id: 4,nivel:1,
 nombre:"Mago",
 tipo: "Soporte",
-vida: 1800,
+vida: 1800,vidaAc:1800,
 ataque:190,
 defensa:90
 }
@@ -57,7 +57,7 @@ var enemigos = [
         id: 1,nivel:1,
         nombre: "Espantapajaros",
         tipo: "practica",
-        vida: 1000,
+        vida: 1000,vidaAc:1000,
         ataque: 0,
         defensa:100
     },
@@ -65,108 +65,108 @@ var enemigos = [
         id: 2,nivel:3,
         nombre: "Enemigo 2",
         tipo: "Normal",
-        vida: 800,
-        ataque: 15,
+        vida: 800,vidaAc:800,
+        ataque: 150,
         defensa:100
     },
     { 
         id: 3,nivel:5,
         nombre: "enemigo 3",
         tipo: "practica",
-        vida: 1000,
-        ataque: 0,
+        vida: 1000,vidaAc:1000,
+        ataque: 200,
         defensa:100
     },
     { 
         id: 4,nivel:10,
         nombre: "Hada",
         tipo: "Soporte",
-        vida: 800,
-        ataque: 15,
+        vida: 800,vidaAc:800,
+        ataque: 1500,
         defensa:100
     },{ 
         id: 5,nivel:15,
         nombre: "enemigo5",
         tipo: "practica",
-        vida: 1000,
-        ataque: 0,
+        vida: 1000,vidaAc:1000,
+        ataque: 500,
         defensa:100
     },
     { 
         id: 6,nivel:20,
         nombre: "Enemigo 6",
         tipo: "Normal",
-        vida: 800,
-        ataque: 15,
+        vida: 800,vidaAc:800,
+        ataque: 1500,
         defensa:100
     },
     { 
         id: 7,nivel:27,
         nombre: "enemigo 7",
         tipo: "practica",
-        vida: 1000,
-        ataque: 0,
+        vida: 1000,vidaAc:1000,
+        ataque: 2100,
         defensa:100
     },
     { 
         id: 8,nivel:40,
         nombre: "Enemigo 8",
         tipo: "Normal",
-        vida: 800,
-        ataque: 15,
+        vida: 8000,vidaAc:800,
+        ataque: 4000,
         defensa:100
     },{ 
         id: 9,nivel:65,
         nombre: "enemigo 9",
         tipo: "practica",
-        vida: 1000,
-        ataque: 0,
+        vida: 60000,vidaAc:1000,
+        ataque: 6000,
         defensa:100
     },
     { 
         id: 10,nivel:100,
         nombre: "Enemigo 10",
         tipo: "Normal",
-        vida: 800,
-        ataque: 15,
+        vida: 10000,vidaAc:800,
+        ataque: 10000,
         defensa:100
     },
     { 
         id: 11,nivel:140,
         nombre: "enemigo 11",
         tipo: "practica",
-        vida: 1000,
-        ataque: 0,
+        vida: 1000,vidaAc:1000,
+        ataque: 14000,
         defensa:100
     },
     { 
         id: 12,nivel:200,
         nombre: "Enemigo 12",
         tipo: "Normal",
-        vida: 800,
-        ataque: 15,
+        vida: 8000,vidaAc:800,
+        ataque: 1500,
         defensa:100
     },{ 
         id: 13,nivel:350,
         nombre: "enemigo 13",
         tipo: "practica",
-        vida: 1000,
-        ataque: 0,
+        vida: 10000,vidaAc:1000,
+        ataque: 2000,
         defensa:100
     },
     { 
         id: 14,nivel:500,
         nombre: "Enemigo 14",
         tipo: "Normal",
-        vida: 800,
-        ataque: 15,
+        vida: 25000,vidaAc:800,
+        ataque:2500,
         defensa:100
     },
     { 
         id: 15,nivel:700,
         nombre: "enemigo 15",
         tipo: "practica",
-        vida: 50000,
+        vida: 50000,vidaAc:50000,
         ataque: 7000,
         defensa:5000
     },
@@ -174,7 +174,7 @@ var enemigos = [
         id: 16,nivel:1000,
         nombre: "Enemigo Final",
         tipo: "Jefe",
-        vida: 80000,
+        vida: 80000,vidaAc:80000,
         ataque: 1500,
         defensa:10000
     },
@@ -217,7 +217,7 @@ function enemigoAc() {
     document.getElementById("ataqueE").textContent=("Ataque: "+enemigoActual.ataque+" Defensa: "+enemigoActual.defensa)
     document.getElementById("defensaE").textContent=("")
 
-    document.getElementById("datosPj").textContent = ("Nivel "+pjActual.nivel);
+    document.getElementById("datosPj").textContent = ("Nivel "+pjActual.nivel+" Oro: "+oros);
     document.getElementById("nombrePj").textContent = (pjActual.nombre);
     document.getElementById("nombrePjI").textContent = (pjActual.nombre);
     document.getElementById("vidaPj").textContent = ("Vida Maxima: "+pjActual.vida);
@@ -233,6 +233,13 @@ function enemigoAc() {
     console.log("Tipo de enemigo:", enemigoActual.tipo);
     console.log("Vida del enemigo:", enemigoActual.vida);
     console.log("Ataque del enemigo:", enemigoActual.ataque);
+
+
+    document.getElementById("enemigo").style.backgroundColor=colorRan();
+    
+
+    recrearBarraDeVidaPj(pjActual.vidaAc, pjActual.vida);
+    recrearBarraDeVida(enemigoActual.vidaAc, enemigoActual.vida);
 }
 
 function seleccionar(n){
@@ -240,6 +247,28 @@ function seleccionar(n){
 npj=n;
 
 enemigoAc()
+
+}
+function pelear(){
+    if(enemigoActual.vida>0){
+    enemigoActual.vida=enemigoActual.vida-pjActual.ataque;
+    pjActual.vida=pjActual.vida-enemigoActual.ataque;
+        
+    }
+    if(enemigoActual.vida<=0){
+     enemigoActual.vida=0;   
+     oros=oros+enemigoActual.nivel;
+     alert(""+enemigoActual.nombre+" ha caido +$"+enemigoActual.nivel)
+    }
+    
+     if(pjActual.vida<0){
+         pjActual.vida=0;   
+         alert(pjActual.nombre+" ha caido");
+        }
+    
+    enemigoAc();
+
+
 
 }
 
@@ -254,3 +283,34 @@ function guardarDatos() {
     localStorage.setItem("level", level);
     localStorage.setItem("exp", exp);
    }
+
+
+
+   function recrearBarraDeVida(vidaMax, vidaActual) {
+    // Obtener el elemento de la barra de vida del DOM
+    var barraDeVida = document.getElementById('barraDeVida');
+
+    // Calcular el porcentaje de vida actual con respecto a la vida máxima
+    var porcentajeVida = (vidaActual / vidaMax) * 100;
+
+    // Actualizar el ancho de la barra de vida para reflejar el porcentaje de vida
+    barraDeVida.style.width = porcentajeVida + '%';
+
+    // También podemos actualizar el texto dentro de la barra de vida, si existe
+    barraDeVida.innerText = vidaActual + ' / ' + vidaMax;
+}
+function recrearBarraDeVidaPj(vidaMax, vidaActual) {
+    // Obtener el elemento de la barra de vida del DOM
+    var barraDeVida = document.getElementById('barraDeVidaPj');
+
+    // Calcular el porcentaje de vida actual con respecto a la vida máxima
+    var porcentajeVida = (vidaActual / vidaMax) * 100;
+
+    // Actualizar el ancho de la barra de vida para reflejar el porcentaje de vida
+    barraDeVida.style.width = porcentajeVida + '%';
+
+    // También podemos actualizar el texto dentro de la barra de vida, si existe
+    barraDeVida.innerText = vidaActual + ' / ' + vidaMax;
+}
+
+
