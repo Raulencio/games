094eef45596c85156128fc3a83f6325a22821f88
Raulencio <47399093+Raulencio@users.noreply.github.com>
2024-06-18 14:24:19 -0400

    Add files via upload

--- js/nuevonuevo.js
+++ js/nuevonuevo.js
@@ -6,23 +6,28 @@ var enemigoac = 2;
 var nenemigo = 5;
 var nPjs = 5;
 var nArmas = 5;
+var dinero = 0;
 var personajeElegido = {
     nombre: "", vidamax: 0, vidaActual: 0, ataque: 0, defensa: 0, probCrit: 0, dmgCrit: 0, recuperacion: 0, vida: 0, alcance: 0, url: ""
 }
 var cual = [
     {
+        comprado: true,
         nombre: "espada de fuego", ataque: 4200, defensa: 400, probCrit: 10, dmgCrit: 30, recuperacion: 50, vida: 5000, alcance: 2
         , url: "https://lh3.googleusercontent.com/9AbUcqcOyLnUcp76PoldOwKDzlpZQg9yUA7HUxBmB0stbQyV1CEYhwgiX6wQvBZ55BU1pVLzytt6Hj8dhqmJhXuzTn0jjObMh0ND68FruMZUJALsYWEcPDBk_w6d2AkDGtzLQ3BnqkjlBPHv5re0DBGeoP0-QBzvEjXWyODtUzoCV-_Bot-7KYwltFYGASNa7TgxRP9ixA5Z8xn7ZpPxp-XRPtOI1NH7nqKJVTd0usz0cz7sQehJTxiCcpeKYvWZUI0zy7LXzu3ErZbs1v7q2pGrsN8I2T6PMovhxxE1_LL9xCS4XhhoD7mTJOxOcIhyjKqX7UpMjZ_iGj4AZE1nDqiUqet5VID5y8tu4exYgJRahJD0qn3ClHyo85Qt-yKhuJYpqp3O0mzt6ctQsuEE3PT_cNQXmEclqY-kogtb0K7JguWyAkNptC6mvyAyQcREzKDOblfVCa4YEAoyLWf-aJUXcOE7_xyEwCB6S-N2L-6TODn2rELVmEy8aLWckKGrTSsl3qTrSgPzo8TF3d-vQ1UxUvotFPEDdjhn0-qxNV_s72gqcyNPCzBrrDPtJ70iOvfQREcy0zbPJ0hwC3Y0iefXjxfJlhWe5PVJzLmhHFl_PltAuHpRFRUF3KvZtBNyXPRblwnMsXCPTbaAzUmQSeATrC-6gSMCndq5iOCZgnr5dwA_kJHf04TQVJBxAzg=w450-h150-no?authuser=0"
     }
     , {
+        comprado: false,
         nombre: "espada de hielo", ataque: 3000, defensa: 200, probCrit: 40, dmgCrit: 20, recuperacion: 50, vida: 2000, alcance: 4
         , url: "https://lh3.googleusercontent.com/qTCY8Ts0_fFUaKAqg1Oo06RTbTKzYlZ-_hcfPTzxD2xfsvYUeGZu_YKWcILimayHQAO8J8KZvAWTagCNQjgN8-hvAamRomecbQZF93zXVXYo_y7vx1bOIqrkYKJa9JhfPbxgUn83jKWeRtPgzGMkm4M1E2qkSZrdRbPxWbScj6LD4XN65mpY2xG1IZgz7NHyn_Dm-okw5h0nYKXVqCRcj1Thzg6jj_R04x2lur9WIAKHEim-z1ZizYODKQL5qwWbZRTisGw4FWe3ArbqV1lWkWkzLIFZ0DbvyraByL05BpYZ0KBSiyaHegTrDao0jxZBQ3F0BThbYEROM1lFqNkzPONvPh7ueNUPT-7JuZCk2qmKHwibYt_Aipa9Dwy-jLQrhrGFNF_GLmdVs65WbsDGOGdMNJ28Mvx2fyMhG_44pMI-R1nS9twy-CZUEgHHNlvaSsQU8McxmX3RuxTMgVEthq8lLIarp8BQJvwwUdgIsH2NOg7_qzwXXA1wdBD0eapyQ3LaLa-CZTq3Le8DXfLaH-ZX-2LjFIJh7bx5Zasro2xq3MPoqDDj65f6C2QtYusg5BKNIscWSQSIr-neqPqHVODwwEmEtNI120aVEh-9J71CB0Db3noYsfOqqZn1TPjqh28tfIAeLtsNzH-t9g5ufA2swXTokMQyvzMHn6_gb8LW92FjaMX4yZ79yI8MLic=w450-h150-no?authuser=0"
     }
     , {
+        comprado: false,
         nombre: "espada del bosque", ataque: 2500, defensa: 1500, probCrit: 20, dmgCrit: 100, recuperacion: 200, vida: 3000, alcance: 1
         , url: "https://lh3.googleusercontent.com/A6zMcmD1AF3VW_pG9GVY3vm6Nuf2W9FZ3MPoRyXi875mgJLghHq-QLjnqm-UYmjkJQ0fsALVCOItgN_GLR2xLEwiYy8WL5LJmmQSgrsdJR02SGvQK_CoyRgX0aGjdJ8t18H5Yed-EGmQD3SowgZWJkWl7JSJdOH1K0Mq6V-RM589IlYT5BlZDdhPvzcPRrRQXapOG-QOj93gpJ9IZi0nLmSbQNLL2_XneuEw3_XSivJj1_cBnpupVWcx-qDgCzJQNkxoMU57p1uV4dZV65rasaySrS6rpE0vkGXS-ZG8gU41IKHuLEo9UiqhDTTxvZSi0pqDUmMIFMFkjfZYdUT0kLhveZ4xgNuwfFsj35-RSF1C4VTBlDGGQzP_7SxTlrwz_pe04i7BewXf9Ecz1eqhqnNjKCjY8YhrI6HkFoogqHVPSURohfHEvy4Y1HWqCDTHQXPgB1qEugvMdeTMaSseq3kE0m7qIMLVaMT2UO7QbjEMRO3P5o2ku-cHs6Ps8ZY_cfuV0vw9TQeV9F6Ad9ieXlw-noeW9Gbug1dY0uMyzRJjQdwCaJ68coDneUJi082xljoDr_saPzpzKy3wancbk1rSXgWOf9iS45skqYdvp5HOhVrqudnAPvozJhyQKpY2FJRLZpomGEZ12_tusGOz-lPX3Hb4csR8d9Tq2Ytf4or5sX98yXK0KcYbB6oy4T0=w450-h150-no?authuser=0"
     }
     , {
+        comprado: false,
         nombre: "katana filosa", ataque: 3500, defensa: 0, probCrit: 50, dmgCrit: 150, recuperacion: 100, vida: 1500, alcance: 1
         , url: "https://lh3.googleusercontent.com/UC3xiyNXJ4-jhQJCIJtuL_h9rtdr1pk-F0yDvvQIAhB9VffPsNbw5h4U9IMr-URpjaq3gKlr4MkApbGODS_cY31V7k6R2M5ytifGjr6ca3Mja8rihoNfvdsR8z_kyeFAAvkFbqCoujPLlEAOtHPmXpHheYSYn79JKYNhdT0yv7mFgt118e-O0-bqO5NYXUPz-W2VOT_X-fNzTLk-zAtI-laJklmGEqwP9xiOd-6cVkSR1Ljckjo8KT9CsR4dMF4Tz4hsmBUak6y9WbmgggSZzdvYcr22aP0pvSYijxzQ3Fw3A7e8yRImDeRaTnEaRyHteL9x29_1xjwfDRACse6yKNZuRfDzu_8j6m0BW6dnV8GtOjMlP5piq82JYEguiSYkOveW63bNfyBjobBixDgtbui-IEacWlxkMndcnqJ0Ql-XKMMb7hCJRgYZSJ04qo5tuMWFfSEIYYDsYBd08onmlRAbGQlFunKIyvRIOcmK9IqKT6vqDK5BT0auvNGaou7E4fjLe2E9RUEgVK53mYS7J5ws3GY_EHkdawSorGYANr0GiuVMDSChlcNPlWcBs5MY6Gx5W4BJySdIffySCPY4H1h-GMVk0DG14n4oplMP2ue9oPw83nmTLNLDc4vuWg9eDyRTaJ6P_MJHgPrO1pKhEEuUCSf_ngKDGRPFvOEjduyHWOMeUlhVW3-po8NgcZo=w450-h150-no?authuser=0"
     }
@@ -30,6 +35,7 @@ var cual = [
 
 var infoPjs = [
     {
+        comprado: false,
         nombre: "Yohiro", vidamax: 3000
         , ataque: 1200, defensa: 700, probCrit: 30
         , dmgCrit: 170, recuperacion: 50
@@ -37,18 +43,21 @@ var infoPjs = [
     }
     ,
     {
+        comprado: false,
         nombre: "Rain", vidamax: 1700
         , ataque: 1800, defensa: 1000, probCrit: 40
         , dmgCrit: 120, recuperacion: 200
         , url: "https://lh3.googleusercontent.com/rByffD1Yc82VjltG_o49spYycjq4v95PWUlhJ_558DSxd259v3kqLVOZOg95jqNUXx-SLC_Ig2WyhPn26JbaX6-4bVaDk9sc0BzMrsV__JQmU9MbvMUKGO_BuMsQ7TyDvj05QqljrOsLakph3FePUVXaBqDNC61zK0Me4dzs9J-tnuc6IFmtBAt5wqGEWURvdQne52SNVYiDRbWBA_zjOhI7YCBA9AfUVirlDR8MJa7txrkz0GLtlvsqbNL0iH8A9A1Q8tVp4J7LPIKIacotz-3_Vru4aaUZiVW2cGyGiiyPaTBLslzdCrvydYPRk060yWcpFFK3Lwg7kGZLxo6mwz9Vf6w7m2shggl8VDZb6ehMAeR-u_r1U3mnCgtvNL8U5FtYiOJkEp-Vj7Pa9trK90otMsE5iV4ZvOu-G-TjADDoAHHAruyQIfDn7Kh0_QkkdQ3dysPl0cfngFeNVV8oUdTpNJqvDw_mUHC06AqhGrXcXltcZLDzFXUDQy3ZcJiPci5eI2h4Fxg49xCWQUMCyFdxMBPZTHW9DyCcQvRSRszUu-SJKkbw1n5P3ajlKkbNRTJpUnOLOarh1iyPy3qppbwtg6Je5aNMspBLKEdG6fLMgzcbRY0ugrLgEqVcHuUO1KF-Qb7AK-2PRzZHfWroifgsFZpl-Jb9S6AThxwdx4x09iWt8ZH0iZFIERWCH44=w50-h100-no?authuser=0"
     }
     , {
+        comprado: false,
         nombre: "Ninja", vidamax: 2500
         , ataque: 1500, defensa: 500, probCrit: 20
         , dmgCrit: 200, recuperacion: 300
         , url: "https://lh3.googleusercontent.com/mNrOQ3HaYykgJsLSzyAvJ1vQZtLmcdyVr1z1ZW4iP_a9l5bso6jojZZXNOlzj-_1uuvUqJysUJmsI9v7NPFOSK_8uHpDp-8NW1wrvEaisPqqg4LGpH2vIgNnSW3YIvB-_iuzIdxtSNb3ahMHTeMzv5D5N0PhbnvEI6ZoNBBPWaJeXqin8v02k3NHb8k23WGLNxJ0KC5ScBLoUjvDEYwPR13idgmX9r8H96qQNor1HGjoSAnM3j6GbKEMwDuwg1j-oMEmtqOSjenjQUaLk0Wl6jdt-SnqDcVgmWz2wNUqepwFm6VFo0aUEYUhXTMC99Z0cnZxbEfr3ftV2CJ7G8fjlFEPs8rHsS4WmO8g9DVlqOqILJwatFtPRTnE64K7D5Me5MT6Mhp37o8i1mh7sLwya3QI4Q_LPFxQ0JxqwhUPbHf5_DS866N1h2q-rQ7HPa7COglXDNanYJSuD8Okrqko13n_-FqaK1r_egxYzYqx5uvZP6aOyu-KB3FaPNBlACwkITnq0Gw8LjZl3PPTJsF3mvuspEO3nCNEF_H8gQPBYShdu8Knn08ope9r4dx3HLYsX7FOJgdwIpV1JJLQUUUKG-KEuL9uyp7R6BZlHJjbCl4l3BeuxGvpccPTfECVpkCbTfc3gmSViWzQPoie9EhU41dGIL1v_4n9QdzjRE5dN9it-qn4_szslEk5kjTL-F0=w50-h100-no?authuser=0"
     }
     , {
+        comprado: false,
         nombre: "Sol", vidamax: 1500
         , ataque: 1700, defensa: 400, probCrit: 10
         , dmgCrit: 250, recuperacion: 100
@@ -111,33 +120,97 @@ var venemigo14 = false;
 var venemigo15 = false;
 var venemigo16 = false;
 
+function actualizarTienda() {
+    document.getElementById("tiendaH").textContent = ("Tienda $" + dinero);
+}
+
+function comprar(o, n) {
+    if (o == 1) {
+        infoPjs[n - 1].comprado = true;
+
+        localStorage.setItem("compradoPj" + n, true);
+    } else if (o == 2) {
+        cual[n - 1].comprado = true;
+        localStorage.setItem("compradoArma" + n, true);
+    }
+}
+
 window.onload = function () {
+
+    actualizarTienda();
+
     botones(1); botones(8);
+
+
+    for (var e = 1; e < nArmas; e++) {
+
+        if (localStorage.getItem("compradoArma" + e) == null) {
+
+            localStorage.setItem("compradoArma" + e, false);
+
+        }
+        if (localStorage.getItem("compradoArma" + e) == true) {
+
+            cual[e - 1].comprado = localStorage.getItem("compradoArma" + e);
+            comprar(2, e);
+
+        }
+        for (var e = 1; e < nPjs; e++) {
+
+            if (localStorage.getItem("compradoPj" + e) == null) {
+
+                localStorage.setItem("compradoPj" + e, false);
+            }
+            if (localStorage.getItem("compradoPj" + e) == true) {
+                infoPjs[e - 1].comprado = localStorage.getItem("compradoPj" + e);
+                comprar(1, e);
+            }
+
+        }
+        comprar(1, 1); comprar(2, 1);
+    }
+
     nombre = localStorage.getItem("nombre");
     if (localStorage.getItem("nombre") == null) {
         nombre = prompt("Por favor, ingrese un nombre:")
         localStorage.setItem("nombre", nombre);
-
     }
     if (localStorage.getItem("armaequipada") != null) {
         narmaequipada = localStorage.getItem("armaequipada");
-
     }
-
     if (localStorage.getItem("pequipado") != null) {
         pequipado = localStorage.getItem("pequipado");
     }
-    document.getElementById("pnombre").textContent = nombre + " nivel:" + nivelActual;
+    document.getElementById("pnombre").textContent = nombre + " nivel: " + nivelActual;
 
     for (var e = 1; e < nArmas; e++) {
 
         $("#arma" + e).append("<p>'" + cual[e - 1].nombre + "'</p>");
         $("#arma" + e).append("<img width='100%' src='" + cual[e - 1].url + "'>");
+
+
+        document.getElementById("arma" + e).style.backgroundColor = "#ffffff";
+
     }
     for (var e = 1; e < nPjs; e++) {
 
         $("#personaje" + e).append("<img width='100%' src='" + infoPjs[e - 1].url + "'>");
+
+
+        document.getElementById("personaje" + e).style.backgroundColor = "#ffffff";
     }
+    for (var e = 1; e < nPjs - 1; e++) {
+
+        $("#pjTienda" + e).append("<img width='100%' src='" + infoPjs[e].url + "'>");
+
+    }
+    for (var e = 1; e < nArmas - 1; e++) {
+
+        $("#pjArma" + e).append("<img width='100%' src='" + cual[e].url + "'>");
+
+    }
+
+
     for (var e = 1; e < nArmas; e++) {
 
         document.getElementById("texto1arma" + e).textContent = ("Ataque: " + cual[e - 1].ataque + "");
@@ -223,33 +296,54 @@ function botones(n) {
 function equipar(n) {
     narmaequipada = n;
 
-    localStorage.setItem("armaequipada", narmaequipada);
+    if (cual[n - 1].comprado == true) {
 
-    $("#armaequipada").empty().append("<img width='100%' src='" + cual[narmaequipada - 1].url + "'>");
-    $("#armaequipada").append('<p>' + cual[narmaequipada - 1].nombre + " Ataque: " + cual[narmaequipada - 1].ataque + " Defensa: " + cual[narmaequipada - 1].defensa + " Prob Crti: " + cual[narmaequipada - 1].probCrit + "</p>");
-    $("#contenedorArma").empty().append("<img width='100%' src='" + cual[narmaequipada - 1].url + "'>");
+        localStorage.setItem("armaequipada", narmaequipada);
 
-    for (var e = 1; e < nArmas; e++) {
-        document.getElementById("arma" + e).style.backgroundColor = '#ffffff';
+        $("#armaequipada").empty().append("<img width='100%' src='" + cual[narmaequipada - 1].url + "'>");
+        $("#armaequipada").append('<p>' + cual[narmaequipada - 1].nombre + " Ataque: " + cual[narmaequipada - 1].ataque + " Defensa: " + cual[narmaequipada - 1].defensa + " Prob Crti: " + cual[narmaequipada - 1].probCrit + "</p>");
+        $("#contenedorArma").empty().append("<img width='100%' src='" + cual[narmaequipada - 1].url + "'>");
+
+        for (var e = 1; e < nArmas; e++) {
+            if (cual[e - 1].comprado == true) {
+                document.getElementById("arma" + e).style.backgroundColor = '#ffffff';
+            } else {
+                document.getElementById("arma" + e).style.backgroundColor = '#000000';
+
+            }
+        }
+
+        document.getElementById("arma" + narmaequipada).style.backgroundColor = '#ffff00';
+
     }
-    document.getElementById("arma" + narmaequipada).style.backgroundColor = '#ffff00';
-
     estadisticas();
 
 }
 function elegir(n) {
+
     pequipado = n;
-    localStorage.setItem("pequipado", pequipado);
-    $("#personajeElegido").empty().append("<img width='100%' src='" + infoPjs[pequipado - 1].url + "'>");
-    $("#contenedorPj").empty().append("<img width='100%' src='" + infoPjs[pequipado - 1].url + "'>");
+
+    if (infoPjs[n - 1].comprado == true) {
+
+        localStorage.setItem("pequipado", pequipado);
+        $("#personajeElegido").empty().append("<img width='100%' src='" + infoPjs[pequipado - 1].url + "'>");
+        $("#contenedorPj").empty().append("<img width='100%' src='" + infoPjs[pequipado - 1].url + "'>");
 
 
-    for (var e = 1; e < nArmas; e++) {
-        document.getElementById("personaje" + e).style.backgroundColor = '#ffffff';
+        for (var e = 1; e < nArmas; e++) {
+            document.getElementById("personaje" + e).style.backgroundColor = '#ffffff';
+
+            if (infoPjs[e - 1].comprado == true) {
+                document.getElementById("personaje" + e).style.backgroundColor = '#ffffff';
+            } else {
+                document.getElementById("personaje" + e).style.backgroundColor = '#000000';
+
+            }
+
+        }
+        document.getElementById("personaje" + pequipado).style.backgroundColor = '#ffff00';
+
     }
-    document.getElementById("personaje" + pequipado).style.backgroundColor = '#ffff00';
-
-
     estadisticas();
 }
 
@@ -364,7 +458,7 @@ var intervalo;
 
 function iniciarRelleno() {
     clearInterval(intervalo);
-    intervalo = setInterval(function() {
+    intervalo = setInterval(function () {
         if (width >= maxWidth) {
             clearInterval(intervalo);
         } else {
@@ -377,9 +471,9 @@ function iniciarRelleno() {
 function consumirEnergia(n) {
     clearInterval(intervalo);
     var consumo = n; // Cantidad de energía a consumir
-    if(width>=consumo){
-    width = Math.max(0, width - consumo);
-    document.getElementById('barra-energia').style.backgroundColor=colorRan();
+    if (width >= consumo) {
+        width = Math.max(0, width - consumo);
+        document.getElementById('barra-energia').style.backgroundColor = colorRan();
     }
     actualizarBarra();
     iniciarRelleno(); // Reinicia el relleno después de consumir
@@ -389,20 +483,21 @@ function actualizarBarra() {
     var barra = document.getElementById('barra-energia');
     barra.style.width = width + '%';
 
-    if(width==25){
+    if (width == 25) {
 
-        barra.style.backgroundColor=colorRan();}
-
-    if(width==50){
-
-        barra.style.backgroundColor=colorRan();
-    }else if(width==75){
-
-        barra.style.backgroundColor=colorRan();
-    }else if(width==100){
-
-        barra.style.backgroundColor=colorRan();
+        barra.style.backgroundColor = colorRan();
     }
-        barra.innerText = width + '/ 100';
+
+    if (width == 50) {
+
+        barra.style.backgroundColor = colorRan();
+    } else if (width == 75) {
+
+        barra.style.backgroundColor = colorRan();
+    } else if (width == 100) {
+
+        barra.style.backgroundColor = colorRan();
+    }
+    barra.innerText = width + '/ 100';
 }
 
